3fb7c1e52b8ab51fd862580a83d29dd25ebad8d9
Trust Okerezi <trustokerezi42@gmail.com>
2021-12-14 11:54:07 +0100

    authentication created

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,47 @@
 const router = require('express').Router();
+const User = require('../models/User')
+const CryptoJS = require('crypto-js')
 
 //TO REGISTER
+router.post('/register', async(req, res) => {
+
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        //encrypt password using CryptoJS
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString(),
+    });
+    
+    try{
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser);
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
+
+//TO LOGIN
+router.post('/login', async(req, res) => {
+    try{    
+        const user = await User.findOne({username: req.body.username});
+
+        !user && res.status(401).json("Wrong credentials!")
+        const hashedPassword = CryptoJS.AES.decrypt(
+            user.password, 
+            process.env.PASS_SEC
+            );
+            //convert password to string
+            const Originalpassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+
+            Originalpassword !==req.body.password && res.status(401).json("Wrong credentials!")
+
+            const {password, ...others} = user._doc;
+
+            res.status(200).json(others);
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
 
 module.exports = router
